83f13a384229533458c6069dc1cd5a1ed238040c
xowls0315 <155832601+xowls0315@users.noreply.github.com>
2024-08-05 14:42:43 +0900

    Add files via upload

---
+++ Day12/dagym/index.js
@@ -0,0 +1,376 @@
+const data = [
+            {
+                "id": "ihhBca5xs",
+                "name": "임의진",
+                "position": "선생님",
+                "gymName": "시청 피트니스온",
+                "gymAddress": "서울 중구 무교동",
+                "profileImage": "https://api.da-gym.net/trainers/images/ihhBca5xs+2Nq4kXKZBZem8rjkCAdKT.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "23nBfgiuo7",
+                "name": "정치운",
+                "position": "선생님",
+                "gymName": "시청 피트니스온",
+                "gymAddress": "서울 중구 무교동",
+                "profileImage": "https://api.da-gym.net/trainers/images/23nBfgiuo7+2QSoJqqiFcup3qdgM6nx3.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "zWTWVDuCp",
+                "name": "김옥철",
+                "position": "선생님",
+                "gymName": "시청 피트니스온",
+                "gymAddress": "서울 중구 무교동",
+                "profileImage": "https://api.da-gym.net/trainers/images/zWTWVDuCp+2S6kYxXuYWYcpfcdCqJns.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": true,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "29PrhKP773",
+                "name": "이준명",
+                "position": "선생님",
+                "gymName": "시청 피트니스온",
+                "gymAddress": "서울 중구 무교동",
+                "profileImage": "https://api.da-gym.net/trainers/images/29PrhKP773+2S62ezfiVtZxCDPku9zrw.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": true,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "2UAv7euiLG",
+                "name": "이태훈",
+                "position": "선생님",
+                "gymName": "광화문 MCT GYM",
+                "gymAddress": "서울 종로구 내수동",
+                "profileImage": "https://api.da-gym.net/trainers/images/2UAv7euiLG+FVLypwK3KG57.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "uMB5JJ9yQ",
+                "name": "이경주",
+                "position": "선생님",
+                "gymName": "광화문 MCT GYM",
+                "gymAddress": "서울 종로구 내수동",
+                "profileImage": "https://api.da-gym.net/trainers/images/uMB5JJ9yQ+HfTkFSuC2Ptk.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": true,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "isefKqeRD",
+                "name": "김한수",
+                "position": "선생님",
+                "gymName": "광화문 MCT GYM",
+                "gymAddress": "서울 종로구 내수동",
+                "profileImage": "https://api.da-gym.net/trainers/images/isefKqeRD+5Ha215VskJ.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": true,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "rfoosx7HU",
+                "name": "나해찬",
+                "position": "선생님",
+                "gymName": "광화문 MCT GYM",
+                "gymAddress": "서울 종로구 내수동",
+                "profileImage": "https://api.da-gym.net/trainers/images/rfoosx7HU+TBbZzwQevD7t.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "5rdCbfFKH3",
+                "name": "하승준",
+                "position": "선생님",
+                "gymName": "회현 더코어필라테스",
+                "gymAddress": "서울 중구 회현동1가",
+                "profileImage": "https://api.da-gym.net/trainers/images/5rdCbfFKH3+KXFUT19xf4eB1DHLEmZ.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "vwJJMLhnE",
+                "name": "박용덕",
+                "position": "선생님",
+                "gymName": "서대문 짐피닉스 PT스튜디오",
+                "gymAddress": "서울 종로구 교남동",
+                "profileImage": "https://api.da-gym.net/trainers/images/vwJJMLhnE+ei5kTZsEybe.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "28tUTBVRsY",
+                "name": "윤대희",
+                "position": "선생님",
+                "gymName": "서대문역 VIP 휘트니스",
+                "gymAddress": "서울 서대문구 충정로2가",
+                "profileImage": "https://api.da-gym.net/trainers/images/28tUTBVRsY+GXZRuKC4zqcj.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "28zSzopA3y",
+                "name": "박경진",
+                "position": "선생님",
+                "gymName": "아현 247휘트니스&필라테스",
+                "gymAddress": "서울 서대문구 북아현동",
+                "profileImage": "https://api.da-gym.net/trainers/images/28zSzopA3y+ExuLVzkQ32d3.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 5,
+                "reviewCount": 1
+            },
+            {
+                "id": "hWyLQbRd1",
+                "name": "유재훈",
+                "position": "선생님",
+                "gymName": "아현 247휘트니스&필라테스",
+                "gymAddress": "서울 서대문구 북아현동",
+                "profileImage": "https://api.da-gym.net/trainers/images/hWyLQbRd1+eabvUtyswUbZcWmsZro.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": true,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "qDensYY5s",
+                "name": "백수민",
+                "position": "선생님",
+                "gymName": "아현 247휘트니스&필라테스",
+                "gymAddress": "서울 서대문구 북아현동",
+                "profileImage": "https://api.da-gym.net/trainers/images/qDensYY5s+2P9SUQjeve716w7xemsru.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "Abv9ft6Wwt",
+                "name": "배진우",
+                "position": "선생님",
+                "gymName": "아현 247휘트니스&필라테스",
+                "gymAddress": "서울 서대문구 북아현동",
+                "profileImage": "https://api.da-gym.net/trainers/images/Abv9ft6Wwt+2Nrts9B5k3zDvhpoyen6c.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "6ZhWgBPUxs",
+                "name": "음지영",
+                "position": "선생님",
+                "gymName": "아현 247휘트니스&필라테스",
+                "gymAddress": "서울 서대문구 북아현동",
+                "profileImage": "https://api.da-gym.net/trainers/images/6ZhWgBPUxs+2YYzz3xNoUvBVY7tQXHAS.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": false,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "2Pfyx8RzhA",
+                "name": "박수란",
+                "position": "선생님",
+                "gymName": "혜화 어메이징휘트니스",
+                "gymAddress": "서울 종로구 명륜1가",
+                "profileImage": "https://api.da-gym.net/trainers/images/2Pfyx8RzhA+2QkpYK1MAg4aRe6JWdXdr.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": true,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "tKvEFkm5b",
+                "name": "김장현",
+                "position": "선생님",
+                "gymName": "혜화 어메이징휘트니스",
+                "gymAddress": "서울 종로구 명륜1가",
+                "profileImage": "https://api.da-gym.net/trainers/images/tKvEFkm5b+2Tgf6VDBYTJUxXW21XvrT.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": true,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "8iS2arNKmt",
+                "name": "양지호",
+                "position": "선생님",
+                "gymName": "혜화 어메이징휘트니스",
+                "gymAddress": "서울 종로구 명륜1가",
+                "profileImage": "https://api.da-gym.net/trainers/images/8iS2arNKmt+2NqRVRjyFVmgR1PCjpZeh.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": true,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            },
+            {
+                "id": "2Kkucuw6wm",
+                "name": "전우영",
+                "position": "선생님",
+                "gymName": "혜화 어메이징휘트니스",
+                "gymAddress": "서울 종로구 명륜1가",
+                "profileImage": "https://api.da-gym.net/trainers/images/2Kkucuw6wm+2QVjySUAmtBSTu8caJ1ES.jpg",
+                "exerciseTypes": [
+                    "P.T"
+                ],
+                "isFreeTrial": false,
+                "isOnetimeLesson": true,
+                "paymentAvailable": true,
+                "rate": 0,
+                "reviewCount": 0
+            }
+        ];
+
+
+// const menu = document.createElement("div");
+// menu.style.display = 'grid';
+// menu.style.gridTemplateColumns = 'repeat(2, 1fr)';
+        
+// data.forEach((x)=> {
+//     const box = document.createElement("div");
+//     // box.innerHTML = `
+//     //     <h4>이름: ${x.name}</h4>
+//     //     <h5>헬스장 이름: ${x.gymName}</h5>
+//     //     <h5>주소: ${x.gymAddress}</h5>
+//     // `;
+//     box.innerHTML = `
+//         <div style="display: flex; align-items: center; margin-bottom: 10px;">
+//             <img src="${x.profileImage}" alt="${x.name}" style="width: 150px; height: 150px; object-fit: cover; margin-right: 30px; border-radius: 10px;">
+//             <div>
+//                 <h3>${x.name} ${x.position}</h3>
+//                 <h5>헬스장 이름: ${x.gymName}</h5>
+//                 <h5>주소: ${x.gymAddress}</h5>
+//                 <h5>${x.exerciseTypes}</h5>
+//             </div>
+//         </div>
+//     `;
+//     box.style.boxShadow = `rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px`;
+//     box.style.display = "flex";
+//     box.style.flexDirection = "column";
+//     box.style.justifyContent = "center";
+//     box.style.alignItems = "center";
+//     box.style.padding = "20px";
+//     menu.appendChild(box);
+// });
+// document.body.appendChild(menu);
+
+
+const container = document.createElement("div");
+container.style.display = "grid";
+container.style.gridTemplateColumns = "repeat(2, 1fr)";
+
+data.forEach((x)=> {
+    const box = document.createElement("div");
+    box.className = "trainer";
+    box.innerHTML = `
+        <div class="imageBox">
+            <img src="${x.profileImage}" alt="" />
+        </div>
+        <div class="info">
+            <span>${x.name}</span>
+            <span>${x.gymAddress}</span>
+        </div>
+    `;
+    container.appendChild(box);
+});
+document.body.appendChild(container)
